a523c22922135bec5262abeb1525cc53e22134a4
mangesh-khaire <157476623+mangesh-khaire@users.noreply.github.com>
2024-09-10 11:46:13 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,13 @@
-//test.java
+public class test {
+    public static void main(String[] args) {
+        // Variables to store the two numbers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Adding the two numbers
+        int sum = num1 + num2;
+
+        // Printing the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
